32191c409cb4061aff454effc080fdeb45ad0921
Oskar Pol <oskarbank@gmail.com>
2017-06-05 20:42:35 +0200

    Edit url

--- src/main/java/pl/oskarpolak/RestTest.java
+++ src/main/java/pl/oskarpolak/RestTest.java
@@ -1,6 +1,5 @@
 package pl.oskarpolak;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -10,13 +9,18 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 
 public class RestTest {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj nazwe usera: " );
+        String username = scanner.nextLine();
+
         try {
             // Otwieramy połączenie HTTP
-            URL url = new URL("http://localhost:8080/api/user/");
+            URL url = new URL("http://localhost:8080/api/user/"+username);
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
             urlConnection.setRequestMethod("GET");
             urlConnection.setRequestProperty("Access-Password", "akademiakodujestfajna");
@@ -34,13 +38,17 @@ public class RestTest {
 
              System.out.println(stringBuilder.toString());
 
-            JSONArray array = new JSONArray(stringBuilder.toString());
+//            JSONArray array = new JSONArray(stringBuilder.toString());
+//
+//            for(int i = 0; i < array.length(); i++) {
+//                JSONObject object = array.getJSONObject(i);
+//                System.out.println("Username: " + object.getString("username"));
+//                System.out.println("Password: " + object.getString("password"));
+//            }
 
-            for(int i = 0; i < array.length(); i++) {
-                JSONObject object = array.getJSONObject(i);
-                System.out.println("Username: " + object.getString("username"));
-                System.out.println("Password: " + object.getString("password"));
-            }
+            JSONObject object = new JSONObject(stringBuilder.toString());
+            System.out.println("Username: " + object.getString("username"));
+            System.out.println("Password: " + object.getString("password"));
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
